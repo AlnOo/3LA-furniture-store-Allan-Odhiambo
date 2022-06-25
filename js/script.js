4602f6f7d1b8a97f1d84875e40e38fe1792a043b
Allan <github email address>
2022-06-25 07:58:17 +0300

    include js fetch for weather api

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
-    const button = document.getElementById('submitBtn');
-    button.addEventListener("click", function() {
-    document.form1.submit();
-    });
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
 
-    const menuEvent = document.getElementsByClassName('logo');
-    menuEvent.addEventListener("click", function() {
-    document.body.style.color = pink
-    });
+
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=8a975fdba5698dfbcba95da718935be0')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("Wrong city name!"));
+})
